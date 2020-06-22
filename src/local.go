880d761e9fc6392b601d7b240a3d4bfc4fc23947
Abraham <cedenoabraham@gmail.com>
2020-06-22 01:45:05 -0300

    modularizing

---
+++ src/local.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+	"strings"
+
+	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing/storer"
+)
+
+var (
+	ErrLoadingEnvFile       = errors.New("Couldn't load the .env file")
+	ErrRetrievingHead       = errors.New("Couldn't bring heads from target branch")
+	ErrNotFound             = errors.New("Repository not found in provided path")
+	ErrDeleteBranch         = errors.New("Sorry, couldn't delete branch")
+	ErrGettingLocalBranches = errors.New("Something bad happened getting local branches")
+)
+
+type Local struct {
+	repository *git.Repository
+}
+
+/**
+
+ */
+func (local *Local) Init() (*git.Repository, error) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		return nil, ErrLoadingEnvFile
+	}
+
+	repository, err := git.PlainOpen(os.Getenv("PATH"))
+	if err != nil {
+		return nil, ErrNotFound
+	}
+
+	return repository, nil
+}
+
+/**
+
+ */
+func (local *Local) GetMergedBranches() ([]string, error) {
+	err := local.checkoutToTarget()
+	if err != nil {
+		panic(err)
+	}
+	references := local.getLocalBranches()
+
+	targetheads := make(map[string]plumbing.Hash)
+	commits, err := local.repository.Log(&git.LogOptions{From: targetheads[os.Getenv("BRANCH")]})
+	if err != nil {
+		return nil, ErrRetrievingHead
+	}
+
+	branches := make(map[string]plumbing.Hash)
+
+	err = references.ForEach(func(branch *plumbing.Reference) error {
+		// The last commit of every branch is stored in branches slice
+		name := branch.Name().Short()
+		head := branch.Hash()
+		branches[name] = head
+
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	merged := make([]string, 0)
+	err = commits.ForEach(func(commit *object.Commit) error {
+		for name, head := range branches {
+			if head.String() == commit.Hash.String() {
+				merged = append(merged, name)
+			}
+		}
+		return nil
+	})
+
+	return merged, nil
+}
+
+/**
+
+ */
+func (local *Local) checkoutToTarget() error {
+	branch := fmt.Sprintf("refs/heads/%s", os.Getenv("BRANCH"))
+	fmt.Println(branch)
+	b := plumbing.ReferenceName(branch)
+
+	wt, err := local.repository.Worktree()
+	if err != nil {
+		panic(err)
+	}
+
+	return wt.Checkout(&git.CheckoutOptions{Create: false, Force: false, Branch: b})
+}
+
+/**
+
+ */
+func (local *Local) getLocalBranches() storer.ReferenceIter {
+	references, err := local.repository.References()
+	if err != nil {
+		panic(err)
+	}
+
+	iter := storer.NewReferenceFilteredIter(func(ref *plumbing.Reference) bool {
+		return strings.Contains(ref.Name().String(), "/heads/") && ref.Name().Short() != "master" && ref.Name().Short() != os.Getenv("BRANCH")
+	}, references)
+
+	return iter
+}
+
+/**
+
+ */
+func (local *Local) DeleteBranches(branches []string) (bool, error) {
+	for _, branch := range branches {
+		err := local.repository.DeleteBranch(branch)
+		if err != nil {
+			return false, ErrDeleteBranch
+		}
+	}
+	return true, nil
+}
--- src/main.go
+++ src/main.go
@@ -4,99 +4,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
-	"gopkg.in/src-d/go-git.v4"
-
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
-	"gopkg.in/src-d/go-git.v4/plumbing/storer"
-
-	"os"
-	"os/user"
-	"strings"
-
 	"github.com/gotk3/gotk3/gtk"
-	. "gopkg.in/src-d/go-git.v4/_examples"
+)
+
+var (
+	local Local
 )
 
 func main() {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	}
-
-	fmt.Println("Welcome to tidy")
-
-	usr, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-
-	r, err := git.PlainOpen(usr.HomeDir + "/shitchat")
-	CheckIfError(err)
-
-	// func (r *Repository) References() (storer.ReferenceIter, error)
-	references, err := r.References()
-	CheckIfError(err)
-
-	Info("Checkout to master branch")
-
-	branch := fmt.Sprintf("refs/heads/%s", os.Getenv("BRANCH"))
-	fmt.Println(branch)
-	b := plumbing.ReferenceName(branch)
-
-	wt, err := r.Worktree()
-	CheckIfError(err)
-	err = wt.Checkout(&git.CheckoutOptions{Create: false, Force: false, Branch: b})
-	CheckIfError(err)
-
-	Info("git branch")
-
-	// NewReferenceFilteredIter returns a reference iterator for the given reference Iterator.
-	// This iterator will iterate only references that accomplish the provided function.
-	iter := storer.NewReferenceFilteredIter(func(ref *plumbing.Reference) bool {
-		return strings.Contains(ref.Name().String(), "/heads/") && ref.Name().Short() != "master"
-	}, references)
-
-	Info("Get master commits")
-
-	masterheads := make(map[string]plumbing.Hash)
-	commits, err := r.Log(&git.LogOptions{From: masterheads["master"]})
-	CheckIfError(err)
-
-	Info("Get branches merged to master")
-
-	branches := make(map[string]plumbing.Hash)
-
-	err = iter.ForEach(func(branch *plumbing.Reference) error {
-		// The last commit of every branch is stored in branches slice
-		name := branch.Name().Short()
-		head := branch.Hash()
-		branches[name] = head
-
-		return nil
-	})
-	CheckIfError(err)
-
-	merged := make([]string, 0)
-	err = commits.ForEach(func(commit *object.Commit) error {
-		for name, head := range branches {
-			if head.String() == commit.Hash.String() {
-				fmt.Printf("Branch %s head (%s) was found in master, so has been merged!\n", name, head)
-				merged = append(merged, name)
-			}
-		}
-		return nil
-	})
-
-	// Delete repository branch
-	/* for _, branch := range merged {
-		fmt.Println("Deleting ", branch, " branch")
-		err := r.DeleteBranch(branch)
-		if err != nil {
-			panic(err)
-		}
-	} */
 
 	// Initialize GTK without parsing any command line arguments.
 	gtk.Init(nil)
