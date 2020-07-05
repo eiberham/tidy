fcd4c9b1bcc4dc420a02214dd845001c23a09234
Abraham <cedenoabraham@gmail.com>
2020-07-04 23:47:15 -0300

    window package

--- src/settings/settings.go
+++ src/settings/settings.go
@@ -7,16 +7,18 @@ import (
 	"os"
 )
 
+// ...
 type Settings struct {
 	Repository Repository `yaml:"repository"`
 }
 
+// ...
 type Repository struct {
 	Folder string `yaml:"folder"`
 	Branch string `yaml:"branch"`
 }
 
-// Open reads already existing configuration from YAML file
+// ...
 func (settings *Settings) Open(filename string) (*Settings, error) {
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -42,7 +44,7 @@ func (settings *Settings) Open(filename string) (*Settings, error) {
 	return config, nil
 }
 
-// Save saves user configuration in YAML file
+// ...
 func (settings *Settings) Save(filename string) (bool, error) {
 	_, err := os.Stat(filename)
 	if os.IsNotExist(err) {
